c4deab2e0581f3165be740fddffc3d964e39cf16
Yoav Caspi <yoavcaspi@gmail.com>
2022-12-08 08:56:43 +0200

    code cleanup

--- day08/part1.py
+++ day08/part1.py
@@ -13,52 +13,34 @@ import support
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-class Directory:
-    def __init__(self, name):
-        self.name = name
-        self.dirs = {}
-        self.size = 0
-        self.files = []
-        self.parent = None
-
-    def add_file(self, size):
-        self.size += size
-        if self.parent:
-            self.parent.add_file(size)
-
-    def add_dir(self, name):
-        d = Directory(name)
-        d.parent = self
-        self.dirs[name] = d
-
-
 def is_visible(board, x1, y1, max_x, max_y):
-    h = board[x1,y1]
+    h = board[x1, y1]
     for x in range(x1):
-        if board[x,y1] >= h:
+        if board[x, y1] >= h:
             break
     else:
         return True
 
     for x in range(x1+1, max_x):
-        if board[x,y1] >= h:
+        if board[x, y1] >= h:
             break
     else:
         return True
 
     for y in range(y1):
-        if board[x1,y] >= h:
+        if board[x1, y] >= h:
             break
     else:
         return True
 
     for y in range(y1+1, max_y):
-        if board[x1,y] >= h:
+        if board[x1, y] >= h:
             break
     else:
         return True
 
     return False
+
 
 def compute(s: str) -> int:
     board = defaultdict(int)
@@ -77,8 +59,7 @@ def compute(s: str) -> int:
                 res+=1
     print_board(board, max_x, max_y)
     return res
-#1552 - Too Low
-#1651 - too low
+
 
 def print_board(board, max_x, max_y):
     for y in range(max_y):
@@ -97,14 +78,14 @@ INPUT_S = '''\
 EXPECTED = 21
 
 
-# @pytest.mark.parametrize(
-#     ('input_s', 'expected'),
-#     (
-#         (INPUT_S, EXPECTED),
-#     ),
-# )
-# def test(input_s: str, expected: int) -> None:
-#     assert compute(input_s) == expected
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
 
 
 def main() -> int:
